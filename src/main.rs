8370722b552de5811043271850a63262ffc88b47
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2024-01-10 01:04:52 -0600

    Add 113 character long line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
+    // This next line is 113 characters long
+    println!("0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF");
 }
